7bf68e949803da67006b06da3de2ddf83ed6f20f
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2022-06-02 23:15:22 +0530

    Angular Project Applications

---
+++ Demo/src/app/marvellous/marvellous.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MarvellousComponent } from './marvellous.component';
+
+describe('MarvellousComponent', () => {
+  let component: MarvellousComponent;
+  let fixture: ComponentFixture<MarvellousComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MarvellousComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MarvellousComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
