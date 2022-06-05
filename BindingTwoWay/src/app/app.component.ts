9d471ea0fa2dc3cd5819f78028696a1c8304aacd
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2022-06-06 01:46:40 +0530

    binding twowayCommunication

---
+++ BindingTwoWay/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+  title = 'BindingTwoWay';
+
+  //public str = "Marvellous";
+  /*public value: any;
+
+  public Send(data: any)
+  {
+    this.value = data;
+  }*/
+
+  public Name: any;
+
+
+}
